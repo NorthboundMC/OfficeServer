781a71e64f4d41b0144cfbc5d833d6623d90c1de
Rosapetals <52610978+Rosapetals@users.noreply.github.com>
2025-03-07 18:20:30 -0600

    first push

---
+++ src/main/java/com/github/rosapetals/officeServer/OfficeServer.java
@@ -0,0 +1,17 @@
+package com.github.rosapetals.officeServer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OfficeServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Works.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
